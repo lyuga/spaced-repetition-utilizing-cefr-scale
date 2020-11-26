83d21a0632008bdec92e9c8a2c4d27c4fd5e4a85
lyuga <58874215+lyuga@users.noreply.github.com>
2020-11-26 12:02:03 +0900

    Initialize Maven project

---
+++ src/main/java/com/lyuga/spaced_repetition_utilizing_cefr_scale/App.java
@@ -0,0 +1,13 @@
+package com.lyuga.spaced_repetition_utilizing_cefr_scale;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/lyuga/spaced_repetition_utilizing_cefr_scale/AppTest.java
@@ -0,0 +1,20 @@
+package com.lyuga.spaced_repetition_utilizing_cefr_scale;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
