c288270406115063ea74ba143245f2555684c305
lyuga <58874215+lyuga@users.noreply.github.com>
2020-11-27 23:26:01 +0900

    CambridgeDictionaryParserの実装

---
+++ src/main/java/com/lyuga/spaced_repetition_utilizing_cefr_scale/CambridgeDictionaryParser.java
@@ -0,0 +1,68 @@
+package com.lyuga.spaced_repetition_utilizing_cefr_scale;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import nu.validator.htmlparser.dom.HtmlDocumentBuilder;
+
+public class CambridgeDictionaryParser {
+	private ArrayList<String> cefrList = new ArrayList<String>();
+
+	public String searchDefinitions(String query) {
+		try {
+			String urlString = "https://dictionary.cambridge.org/dictionary/english/"
+					+ URLEncoder.encode(query, "UTF-8");
+			URL url = new URL(urlString);
+
+			// 接続
+			URLConnection connection = url.openConnection();
+			connection.connect();
+
+			InputStream inputStream = connection.getInputStream();
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+			BufferedReader reader = new BufferedReader(inputStreamReader);
+
+			// DOMツリーの構築
+			HtmlDocumentBuilder builder = new HtmlDocumentBuilder();
+			Document document = builder.parse(new InputSource(reader));
+
+			XPath xPath = XPathFactory.newInstance().newXPath();
+			// XPath式内で接頭辞 h がついている要素を HTML の要素として認識させる
+			xPath.setNamespaceContext(new NamespaceContextHTML());
+
+			// CEFRレベルが含まれたNodeをすべて取得
+			NodeList cefrNodeList = (NodeList) xPath.evaluate("//*[starts-with(@class, 'epp-xref dxref')]", document,
+					XPathConstants.NODESET);
+
+			// Nodeごとに文字列を取得し、可変長配列であるcefrListへ追加
+			for (int i = 0; i < cefrNodeList.getLength(); i++) {
+				Node cefrNode = cefrNodeList.item(i);
+				cefrList.add(cefrNode.getTextContent());
+			}
+
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// cefrListから、最も低いCEFRレベルを取り出して返却
+		return Collections.min(cefrList);
+	}
+}
